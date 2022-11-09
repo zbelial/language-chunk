35f14fdd41820c71241a151807080ee3bd7ee989
zbelial <zjytj@qq.com>
2022-11-09 18:48:50 +0800

    wip

---
+++ lc-memo.el
@@ -0,0 +1,37 @@
+;;; lc-memo.el --- Package used to help capture and remember English words and chunks. -*- lexical-binding: t -*-
+
+;; Author: zbelial(zjyzhaojiyang@gmail.om)
+;; Maintainer: zbelial
+;; Version: 0.1
+;; Package-Requires: (dependencies)
+;; Homepage: https://github.com/zbelial/language-chunk
+;; Keywords: english
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; commentary
+
+;;; Code:
+
+(message "Hello World!")
+
+(provide 'lc-memo)
+
+;;; lc-memo.el ends here
